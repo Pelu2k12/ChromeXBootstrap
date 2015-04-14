c567e7b4462504950f3699ed1346de00880c9b93
Arian Caraballo <arian.caraballo@bookit.com>
2015-04-14 17:51:16 -0400

    open in a new window

--- js/app/app.js
+++ js/app/app.js
@@ -4,4 +4,14 @@
     $('.panel-heading').on('click', function () {
         localStorage.stepsSelectedAccordion = this.nextElementSibling.id;
     });
+    
+    var popUpWindow = localStorage.isPopUpWindowOpen || false;
+    $('#popupwindow').on('click', function (){
+        this.style.display = 'none';
+        popUpWindow = window.open("", "STEPS");
+        if (!localStorage.isPopUpWindowOpen){
+            popUpWindow.document.write(document.documentElement.innerHTML);
+            localStorage.isPopUpWindowOpen = true;
+        }
+    });
 })(jQuery);
